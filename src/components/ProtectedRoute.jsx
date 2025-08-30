33c3830ac76e67905c4a02c5a58b8dc9c4d68bdd
t501prods <t501haase@gmail.com>
2025-08-30 19:36:17 +0100

    protectedroute updated

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // src/ProtectedRoute.jsx
 import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { account } from "./lib/appwrite";
+import { account } from "../lib/appwrite";
 
 export default function ProtectedRoute({ children }) {
   const [ok, setOk] = useState(null);
